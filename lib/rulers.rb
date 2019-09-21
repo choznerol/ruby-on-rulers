4ab4c42bdd28157fc39fa018dc638dde97ec0d82
Lawrence Chou <lawrencechou1024@gmail.com>
2019-09-21 10:55:52 +0800

    feat(Rulers::Application): print 'env' in <details>

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,10 +1,20 @@
 require "rulers/version"
+require 'json'
 
 module Rulers
   class Application
     def call(env)
       [200, {'Content-Type' => 'text/html'},
-       ["Yay! You're on Rulers!"]]
+       ["
+<h1> Yay! You're on Rulers! </h1>
+
+<br/>
+
+<details>
+  <summary> env </summary>
+  <pre>#{JSON.pretty_generate(env)}</pre>
+</details>
+"]]
     end
   end
 end
