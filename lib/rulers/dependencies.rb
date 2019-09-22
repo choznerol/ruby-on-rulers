9668e32a226fcbc36bd9efc76a1acd1b0c9d9d1d
Lawrence Chou <lawrencechou1024@gmail.com>
2019-09-22 14:53:50 +0800

    feat(dependencies): 只對從 Rulers#call 觸發的 const_missing 嘗試 auto-loading

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -1,6 +1,17 @@
 class Object
   def self.const_missing(c)
-    STDERR.puts "[AUTOLOAD] const_missing(#{c.inspect})"
+    STDOUT.puts "[AUTOLOAD] const_missing(#{c.inspect})"
+
+    # 只對從 Rulers#call 觸發的 const_missing 嘗試 auto-loading
+    # 避免劫持 Rulers 與 app 以外地方的所有 const_missing，例如 Rulers 主程式中其他地方用到的第三方套件
+    is_from_rulers_call_method = caller_locations.any? do |line|
+      line.to_s['lib/rulers.rb'] && line.to_s['in `call\''] # e.g. lib/rulers.rb:42 in `call'
+    end
+    unless is_from_rulers_call_method
+      STDERR.puts "[AUTOLOAD] 由於 callstack 不包含 Rulers#call，不對 #{c.inspect} 嘗試 auto-loading"
+      return super(c)
+    end
+
     @calling_const_missing ||= {}
 
     return nil if @calling_const_missing[c]
